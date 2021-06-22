441725aeff49ebefdf45c29e0714981f5da4ba84
xbl4z3r <chrispetroniu@gmail.com>
2021-06-22 09:54:41 +0300

    Added Config file, Assets and App.xaml and it's components

---
+++ App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+using System.Windows;
+using HyperHack;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Newtonsoft.Json.Linq;
+
+namespace HyperHack
+{
+	public partial class App : Application
+	{
+
+		[DebuggerNonUserCode]
+		[GeneratedCode("PresentationBuildTasks", "5.0.3.0")]
+		public void InitializeComponents()
+		{
+			base.StartupUri = new Uri("Window1.xaml", UriKind.Relative);
+		}
+
+		[STAThread]
+		[DebuggerNonUserCode]
+		[GeneratedCode("PresentationBuildTasks", "5.0.3.0")]
+		public static void AppMain()
+		{
+			App app = new App();
+			app.InitializeComponents();
+			app.Run();
+		}
+	}
+}
